206d5b996f9dba3d9e0fc4e4c8d23218ee08cbe8
HamishDonaldson <hamish.donaldson@halo-medical.co.uk>
2024-10-15 14:50:49 +0100

    bumped version number

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import tkinterdnd2
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 # Version number
-VERSION = "1.3.0 Pre-Release"
+VERSION = "1.2.1"
 
 # To fix blurriness on some displays
 try:
