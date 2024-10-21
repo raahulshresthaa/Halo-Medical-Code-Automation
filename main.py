cdc376efd432d839e9ebbaef9f8e63716f3f5f00
HamishDonaldson <hamish.donaldson@halo-medical.co.uk>
2024-10-21 14:26:41 +0100

    bug fix afo reader

--- main.py
+++ main.py
@@ -735,7 +735,7 @@ result_text.dnd_bind('<<Drop>>', handle_drop)
 # Define model IDs (replace with your actual model IDs)
 model_ids = {
     'Insoles': 'insoleFormV5',  # model id's
-    'AFOs': 'AfoReaderV2'   
+    'AFOs': 'AfoReaderV3'   
 }
 
 # Set up the model_id_var with default value
