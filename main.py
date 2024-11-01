98088ffebc0751786d6e267bf3397a3397d3e974
HamishDonaldson <hamish.donaldson@halo-medical.co.uk>
2024-11-01 17:02:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -531,8 +531,35 @@ class PdfButtonHandler:
             else:
                 passed_codes.append('B56')
 
-        # Insole modelling section
+        # Insole modelling section - to do
         if content_dict.get('left 1st addition') in ('valgus pad', 'metatarsal pad', 'metatarsal bar', 'balance pad', 'heel pad', 'cuboid pad', 'cobra pad', 'neuroma pad', 'sulcus crest', 'arch fill'):
+            if insole_type == 'cradle':
+                passed_codes.append('A45')
+            else:
+                passed_codes.append('B41')
+
+        if content_dict.get('left 1st addition') in ('morton\'s extension', 'reverse morton\'s extension', 'poron forefoot'):
+            if insole_type == 'cradle':
+                passed_codes.append('A45')
+            else:
+                passed_codes.append('B56')
+
+        if content_dict.get('left 1st addition') in ('kinetic wedge', 'heel raise'):
+            if insole_type == 'cradle':
+                passed_codes.append('A45')
+            else:
+                passed_codes.append('B43')
+
+        if content_dict.get('left 1st addition') in ('morton\'s extension', 'reverse morton\'s extension', 'poron forefoot'):
+            if insole_type == 'cradle':
+                passed_codes.append('A45')
+            else:
+                passed_codes.append('B56')
+
+        if content_dict.get('left 1st addition') in ('neurological footplate'):
+            passed_codes.append('D8A') # to do need to finish the rest of the codes for 1st addition ect
+
+        if content_dict.get('right 1st addition') in ('valgus pad', 'metatarsal pad', 'metatarsal bar', 'balance pad', 'heel pad', 'cuboid pad', 'cobra pad', 'neuroma pad', 'sulcus crest', 'arch fill'):
             if insole_type == 'cradle':
                 passed_codes.append('A45')
             else:
