3e16c34c318b6d482d49f89375d1f06c7fc76611
HamishDonaldson <hamish.donaldson@halo-medical.co.uk>
2024-12-11 10:42:19 +0000

    reverted version number

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from tkinterdnd2 import DND_FILES, TkinterDnD
 from collections import defaultdict
 
 # Version number
-VERSION = "4.2.0-dev"
+VERSION = "4.1.0"
 
 
 # To fix blurriness on some displays
