51773af6b3f312cb78ef044b125138b141d952be
HamishDonaldson <hamish.donaldson@halo-medical.co.uk>
2025-09-21 20:21:57 +0100

    bumped version

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ VERSION = "7.0.0-alpha"
 MODEL_IDS = {
     'Insoles': 'InsoleFullReaderV13',
     'AFOs': 'AfoReaderV11',
-    'Bespoke': 'BespokeReaderFullV15',
+    'Bespoke': 'BespokeReaderFullV16',
     'Modular': 'ModularReaderFullV6',
     'A&R': 'AdaptsAndRepairsReader3',
     'Kafo': 'KafoFormReaderV3' 
